a25bbf94ecded765ee5033a5f45a30a1dd1cf38b
PedroMM98 <pedro27mm2012@gmail.com>
2025-06-17 21:38:46 -0500

    Adding ci-cd workflows fix lower

--- operation/utils.py
+++ operation/utils.py
@@ -3,7 +3,7 @@ import os
 DEFAULT_BUCKET = "mlops-utec"
 ENV_CODE = "prod"
 TRACKING_SERVER_ARN = 'arn:aws:sagemaker:us-east-1:654654589924:mlflow-tracking-server/mlops-utec-mlflow-server'
-USERNAME = os.getenv("GITHUB_ACTOR")
+USERNAME = os.getenv("GITHUB_ACTOR").lower()
 
 
 
